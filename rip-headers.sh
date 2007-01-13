923a40e1d146cef01d4c0a01dbd9ded58fb9bf01
Mike Frysinger <vapier@gentoo.org>
2007-01-13 19:19:12 +0000

    fake out the fixdeps script so people can emerge linux-headers without linux-headers

--- rip-headers.sh
+++ rip-headers.sh
@@ -13,13 +13,21 @@ mkdir ${dst}/include
 cp ${src}/include/Kbuild ${dst}/include/
 cp -r $(find ${src}/include -mindepth 2 -maxdepth 2 -name Kbuild -printf %h' ') ${dst}/include/
 mkdir ${dst}/scripts
-cp -r ${src}/scripts/{Makefile,Kbuild}* ${src}/scripts/unifdef.c ${src}/scripts/basic ${dst}/scripts/
+cp -r \
+	${src}/scripts/{Makefile,Kbuild}* \
+	${src}/scripts/unifdef.c \
+	${src}/scripts/*.sh \
+	${dst}/scripts/
+mkdir -p ${dst}/scripts/basic
+printf '#!/bin/sh\nexit 0' > ${dst}/scripts/basic/fixdep
+chmod a+rx ${dst}/scripts/basic/fixdep
+touch ${dst}/scripts/basic/Makefile
 mkdir ${dst}/arch
 arches=$(cd ${src}/arch ; ls)
 for a in ${arches} ; do
 	[[ ! -e ${src}/include/asm-${a}/Kbuild ]] && continue
 	mkdir -p ${dst}/arch/${a}
-	cp ${src}/arch/${a}/Makefile ${dst}/arch/${a}/
+	cp ${src}/arch/${a}/Makefile* ${dst}/arch/${a}/
 done
 
 cp README.ripped-headers rip-headers.sh ${dst}/
