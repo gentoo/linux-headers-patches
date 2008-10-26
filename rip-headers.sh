b9de7168a82aaaf61759c27faeef95fe7e7d5c91
Mike Frysinger <vapier@gentoo.org>
2008-10-26 10:48:57 +0000

    add support for 2.6.27 header split

--- rip-headers.sh
+++ rip-headers.sh
@@ -2,6 +2,8 @@
 
 ver=$1
 [[ -z $ver ]] && exit 1
+[[ ${ver} == linux-* ]] && ver=${ver#linux-}
+ver=${ver%/}
 
 src=linux-${ver}
 dst=gentoo-headers-base-${ver}
@@ -16,7 +18,7 @@ mkdir ${dst}/scripts
 cp -r \
 	${src}/scripts/{Makefile,Kbuild}* \
 	${src}/scripts/unifdef.c \
-	${src}/scripts/*.sh \
+	${src}/scripts/*.{sh,pl} \
 	${dst}/scripts/
 mkdir -p ${dst}/scripts/basic
 printf '#!/bin/sh\nexit 0' > ${dst}/scripts/basic/fixdep
@@ -25,13 +27,18 @@ touch ${dst}/scripts/basic/Makefile
 mkdir ${dst}/arch
 arches=$(cd ${src}/arch ; ls)
 for a in ${arches} ; do
-	[[ ! -e ${src}/include/asm-${a}/Kbuild ]] && continue
-	mkdir -p ${dst}/arch/${a}
-	cp ${src}/arch/${a}/Makefile* ${dst}/arch/${a}/
+	if [[ -e ${src}/include/asm-${a}/Kbuild ]] || [[ -e ${src}/arch/${a}/include/asm/Kbuild ]] ; then
+		mkdir -p ${dst}/arch/${a}
+		cp ${src}/arch/${a}/Makefile* ${dst}/arch/${a}/
+		if [[ -e ${src}/arch/${a}/include ]] ; then
+			cp -r ${src}/arch/${a}/include ${dst}/arch/${a}/
+		fi
+	fi
 done
 
 cp README.ripped-headers rip-headers.sh ${dst}/
 
-tar cf - ${dst} | lzma > ${dst}.tar.lzma
+compress=lzma
+tar cf - ${dst} | ${compress} > ${dst}.tar.lzma
 rm -rf ${dst}
 ls -lh ${dst}.tar.lzma
